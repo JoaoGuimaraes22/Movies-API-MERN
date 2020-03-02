5eca4d90c85ac501f7c39525b5e89d2d873f3d8e
Joao Guimaraes <jssgmrs22@gmail.com>
2020-03-02 15:48:23 +0000

    cron job change

--- movies-insert-mongo/index.js
+++ movies-insert-mongo/index.js
@@ -1,8 +1,9 @@
 var CronJob = require("cron").CronJob;
 const mongo = require("./db/mongo");
 
+// Runs at 18h00
 var job = new CronJob(
-  "* * * * *",
+  "* 1 */2 * *",
   () => {
     mongo();
   },
